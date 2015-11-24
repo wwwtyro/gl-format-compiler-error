8a54dbb7790f17eda16e67a4ff182382035bb582
Rye Terrell <ryeterrell@ryeterrell.net>
2015-11-24 11:16:56 -0600

    use add-line-numbers module and throw an error when the line parsing fails.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var sprintf = require('sprintf-js').sprintf;
 var glConstants = require('gl-constants/lookup');
 var shaderName = require('glsl-shader-name');
+var addLineNumbers = require('add-line-numbers');
 
 module.exports = formatCompilerError;
 
@@ -9,31 +10,34 @@ function formatCompilerError(errLog, src, type) {
     "use strict";
 
     var name = shaderName(src) || 'of unknown name (see npm glsl-shader-name)';
-    
+
     var typeName = 'unknown type';
     if (type !== undefined) {
         typeName = type === glConstants.FRAGMENT_SHADER ? 'fragment' : 'vertex'
     }
 
-    var longForm = sprintf("Error in %s shader %s:\n", typeName, name);
+    var longForm = sprintf('Error compiling %s shader %s:\n', typeName, name);
     var shortForm = sprintf("%s%s", longForm, errLog);
 
     var errorStrings = errLog.split('\n');
-
     var errors = {};
 
     for (var i = 0; i < errorStrings.length; i++) {
         var errorString = errorStrings[i];
+        if (errorString === '') continue;
         var lineNo = parseInt(errorString.split(':')[2]);
+        if (isNaN(lineNo)) {
+            throw new Error(sprintf('Could not parse error: %s', errorString));
+        }
         errors[lineNo] = errorString;
     }
 
-    var lines = src.split('\n');
+    var lines = addLineNumbers(src).split('\n');
 
     for (var i = 0; i < lines.length; i++) {
         if (!errors[i+3] && !errors[i+2] && !errors[i+1]) continue;
         var line = lines[i];
-        longForm += sprintf("%4d: %s\n", i+1, line);
+        longForm += line + '\n';
         if (errors[i+1]) {
             var e = errors[i+1];
             e = e.substr(e.split(':', 3).join(':').length + 1).trim();
@@ -42,8 +46,8 @@ function formatCompilerError(errLog, src, type) {
     }
 
     return {
-        long: longForm,
-        short: shortForm
+        long: longForm.trim(),
+        short: shortForm.trim()
     };
 }
 
